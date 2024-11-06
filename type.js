ac7a9d96d1a1500e9b5fe92ca537ea6a1b6ff361
ysh <ysh940129@gmail.com>
2024-11-06 14:35:53 +0900

    2024. 11. 6

---
+++ type.js
@@ -0,0 +1,19 @@
+// 참&거짓
+// let isDone: boolean = true;
+// console.log(isDone);
+// 숫자
+// let ten: number = 10;
+// console.log(ten);
+// 문자
+// let language: string = "kor";
+// console.log(language);
+//[] 배열
+var list = [12, 46, 24];
+console.log(list);
+// 제네릭
+var apb = ["a", "b", "c"];
+console.log(apb);
+// 튜플
+var multy;
+multy: ["qwer", 1234];
+multy: [4567, "zxcv"];
---
+++ type.ts
@@ -0,0 +1,25 @@
+// 참&거짓
+// let isDone: boolean = true;
+// console.log(isDone);
+
+// 숫자
+// let ten: number = 10;
+// console.log(ten);
+
+// 문자
+// let language: string = "kor";
+// console.log(language);
+
+//[] 배열
+let list: number[] = [12, 46, 24];
+console.log(list);
+
+// 제네릭
+let apb: Array<string> = ["a", "b", "c"];
+console.log(apb);
+
+// 튜플
+let multy: [string, number];
+
+multy = ["qwer", 1234];
+multy = [4567, "zxcv"];
