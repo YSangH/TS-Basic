4b803a78fe667fb6f79ff5adcfd5a24881f993ba
ysh <ysh940129@gmail.com>
2024-11-08 12:43:40 +0900

    fix:interface.ts

--- interface.js
+++ interface.js
@@ -1,5 +1,7 @@
 var IF = "-------- 인터페이스 ---------";
 var SP = "------ 선택적 프로퍼티 ------";
+var RP = "------- 읽기 프로퍼티 -------";
+var EPC = "----- 초과 프로퍼티 검사 -----";
 console.log(IF);
 function print(labelObj) {
     console.log(labelObj.label);
@@ -19,3 +21,20 @@ function createSquare(config) {
 }
 var mySquare = createSquare({ color: "black" });
 console.log(mySquare);
+console.log(RP);
+var x1 = { x: "number", y: 123 };
+// x1.x = "apb"; // 오류
+// x1.y = 5; // 오류
+console.log(x1);
+var a = [1, 2, 3, 4];
+var arr = a;
+console.log(arr);
+console.log(EPC);
+function createSquare2(config) {
+    // ...
+}
+// let mySquare = createSquare({ colour: "red", width: 100 }); // 현재상태 오류 발생
+var mySquare2 = createSquare2({ width: 100, opacity: 0.5 });
+var squareOptions = { color: "red", width: 200 };
+var Square = createSquare(squareOptions);
+console.log(Square);
--- interface.ts
+++ interface.ts
@@ -1,6 +1,7 @@
 let IF = "-------- 인터페이스 ---------";
 let SP = "------ 선택적 프로퍼티 ------";
-
+let RP = "------- 읽기 프로퍼티 -------";
+let EPC = "----- 초과 프로퍼티 검사 -----";
 
 console.log(IF);
 // 인터페이스: 객체가 어떤 속성과 메서드를 가져야 하는지 지정
@@ -37,3 +38,40 @@ function createSquare(config: SquareConfig): {color: string; area: number} {
 
 let mySquare = createSquare({ color: "black" });
 console.log(mySquare);
+
+
+console.log(RP);
+// 읽기 프로퍼티 : 할당 후 수정 불가인 속성, 앞에 readonly을 넣는다.
+interface read{
+    readonly x: string;
+    readonly y: number;
+}
+
+let x1: read = { x: "number", y: 123 };
+// x1.x = "apb"; // 오류
+// x1.y = 5; // 오류
+console.log(x1);
+
+let a: number[] = [1, 2, 3, 4];
+let arr: ReadonlyArray<number> = a;
+
+console.log(arr);
+
+console.log(EPC);
+interface SquareConfig {
+    color?: string;
+    width?: number;
+    [propName: string]: any;
+}
+
+function createSquare2(config: SquareConfig): { color: string; area: number } {
+    // ...
+}
+
+// let mySquare = createSquare({ colour: "red", width: 100 }); // 현재상태 오류 발생
+let mySquare2 = createSquare2({ width: 100, opacity: 0.5 } as SquareConfig);
+
+let squareOptions = { color: "red", width: 200 };
+let Square = createSquare(squareOptions);
+
+console.log(Square);
