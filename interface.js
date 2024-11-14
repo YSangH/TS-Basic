804d5c53aba63cc32cf78d8c75de3f63eaecc824
ysh <ysh940129@gmail.com>
2024-11-14 16:34:16 +0900

    2024 11 14

--- interface.js
+++ interface.js
@@ -2,12 +2,14 @@ var IF = "-------- 인터페이스 ---------";
 var SP = "------ 선택적 프로퍼티 ------";
 var RP = "------- 읽기 프로퍼티 -------";
 var EPC = "----- 초과 프로퍼티 검사 -----";
+var FT = "-------- 함수 타입 --------";
+var IT = "--------- 인덱서블 타입 --------";
 console.log(IF);
-function print(labelObj) {
+function printLabel(labelObj) {
     console.log(labelObj.label);
 }
-var myObj = { size: 10, label: "size 10 object" };
-print({ label: myObj });
+var myObj = { size: 10, label: "size 10 object" }; // labelValue 타입으로 정의
+printLabel({ label: myObj });
 console.log(SP);
 function createSquare(config) {
     var newSquare = { color: "white", area: 100 };
@@ -31,10 +33,37 @@ var arr = a;
 console.log(arr);
 console.log(EPC);
 function createSquare2(config) {
-    // ...
+    var color = config.color || "white";
+    var width = config.width || 10;
+    var area = width * width;
+    return { color: color, area: area };
 }
 // let mySquare = createSquare({ colour: "red", width: 100 }); // 현재상태 오류 발생
-var mySquare2 = createSquare2({ width: 100, opacity: 0.5 });
+// let mySquare2 = createSquare2({ width: 100, opacity: 0.5 } as SquareConfig);
 var squareOptions = { color: "red", width: 200 };
 var Square = createSquare(squareOptions);
 console.log(Square);
+console.log(FT);
+var add = function (x, y) { return x + y; };
+var subtract = function (x, y) { return x - y; };
+function operate(a, b, operation) {
+    return operation(a, b);
+}
+// 출력 결과
+console.log(add(5, 3)); // 출력: 8
+console.log(subtract(5, 3)); // 출력: 2
+var calculator = {
+    name: "Simple Calculator",
+    add: function (x, y) { return x + y; },
+    subtract: function (x, y) { return x - y; },
+};
+// 출력 결과
+console.log(calculator.name); // 출력: "Simple Calculator"
+console.log(calculator.add(10, 5)); // 출력: 15
+console.log(calculator.subtract(10, 5)); // 출력: 5
+console.log(IT);
+var myArray = ["Hello", "World"]; // 0번 인덱스 "Hello", 1번 인덱스 "World" 
+console.log(myArray[0]); // 출력: "Hello"
+var myDictionary = { length: 5, width: 10 };
+console.log(myDictionary["length"]); // 출력: 5
+// 주의 : 키 타입은 string 또는 number만 허용
--- interface.ts
+++ interface.ts
@@ -2,20 +2,22 @@ let IF = "-------- 인터페이스 ---------";
 let SP = "------ 선택적 프로퍼티 ------";
 let RP = "------- 읽기 프로퍼티 -------";
 let EPC = "----- 초과 프로퍼티 검사 -----";
+let FT = "-------- 함수 타입 --------";
+let IT = "--------- 인덱서블 타입 --------";
 
 console.log(IF);
 // 인터페이스: 객체가 어떤 속성과 메서드를 가져야 하는지 지정
-interface labelValue{
+interface labelValue {
     size: number;
     label: string;
 }
 
-function print(labelObj: { label: labelValue }) {
+function printLabel(labelObj: { label: labelValue }) {
     console.log(labelObj.label);
 }
 
-let myObj = { size: 10, label: "size 10 object" };
-print({ label: myObj });
+let myObj: labelValue = { size: 10, label: "size 10 object" }; // labelValue 타입으로 정의
+printLabel({ label: myObj });
 
 console.log(SP);
 // 선택적 프로퍼티 : 객체 안의 몇 개의 프로퍼티만 채워 함수에 전달
@@ -57,21 +59,80 @@ let arr: ReadonlyArray<number> = a;
 
 console.log(arr);
 
+
 console.log(EPC);
 interface SquareConfig {
     color?: string;
     width?: number;
-    [propName: string]: any;
+    [propName: string]: any; // 추가 프로퍼티 허용
 }
 
-function createSquare2(config: SquareConfig): { color: string; area: number } {
-    // ...
+function createSquare2(config: SquareConfig): { color: string, area: number } {
+    const color = config.color || "white";
+    const width = config.width || 10;
+    const area = width * width;
+    return { color, area };
 }
 
 // let mySquare = createSquare({ colour: "red", width: 100 }); // 현재상태 오류 발생
-let mySquare2 = createSquare2({ width: 100, opacity: 0.5 } as SquareConfig);
+// let mySquare2 = createSquare2({ width: 100, opacity: 0.5 } as SquareConfig);
 
-let squareOptions = { color: "red", width: 200 };
+let squareOptions = { color: "red", width: 200 } as SquareConfig;
 let Square = createSquare(squareOptions);
 
 console.log(Square);
+
+console.log(FT);
+// 기본 구문 : 함수의 매개변수와 반환 타입을 지정하는 호출 시그니처를 사용
+ interface MathOperation {
+    (a: number, b: number): number;
+}
+
+const add: MathOperation = (x, y) => x + y;
+const subtract: MathOperation = (x, y) => x - y;
+
+function operate(a: number, b: number, operation: MathOperation): number {
+    return operation(a, b);
+}
+
+// 출력 결과
+console.log(add(5, 3)); // 출력: 8
+console.log(subtract(5, 3)); // 출력: 2
+
+// 응용
+interface Calculator {
+    name: string;   // 이름 타입
+    add: MathOperation; // 더하기 타입
+    subtract: MathOperation;    // 빼기 타입
+}
+
+const calculator: Calculator = {
+    name: "Simple Calculator",
+    add: (x, y) => x + y,
+    subtract: (x, y) => x - y,
+};
+
+// 출력 결과
+console.log(calculator.name); // 출력: "Simple Calculator"
+console.log(calculator.add(10, 5)); // 출력: 15
+console.log(calculator.subtract(10, 5)); // 출력: 5
+
+console.log(IT);
+
+//구문 : [key: KeyType]: ValueType 형태
+interface StringArray {
+    [index: number]: string; // 인덱스는 숫자, 키값은 문자
+}
+
+let myArray: StringArray = ["Hello", "World"]; // 0번 인덱스 "Hello", 1번 인덱스 "World" 
+console.log(myArray[0]); // 출력: "Hello"
+
+interface NumberDictionary {
+    [key: string]: number; // 인덱스는 문자, 키값은 숫자
+    length: number; // 추가적인 프로퍼티도 정의 가능
+}
+
+let myDictionary: NumberDictionary = { length: 5, width: 10 };
+console.log(myDictionary["length"]); // 출력: 5
+
+// 주의 : 키 타입은 string 또는 number만 허용
